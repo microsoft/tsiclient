2149a30926fe0c10eb452035914216c4803cb013
Irem Yildirim <iryildir@microsoft.com>
2019-04-18 15:45:40 -0700

    context menu added

--- src/UXClient/Components/HierarchyNavigation/HierarchyNavigation.ts
+++ src/UXClient/Components/HierarchyNavigation/HierarchyNavigation.ts
@@ -8,14 +8,18 @@ import { ChartOptions } from '../../Models/ChartOptions';
 class HierarchyNavigation extends Component{
     private server: ServerClient;
     private wrapper;
-    private usedHierarchyContToken;
-    private usedInstancesContToken;
+    private clickedInstance;
     public chartOptions: ChartOptions = new ChartOptions();
 
-    1
     constructor(renderTarget: Element){ 
         super(renderTarget); 
         this.server = new ServerClient();
+        d3.select("html").on("click." + Utils.guid(), () => {
+            if (this.clickedInstance && d3.event.target != this.clickedInstance.contextMenu) {
+                this.closeContextMenu();
+                this.clickedInstance = null;
+            }
+        })
     }
 
     HierarchyNavigation(){
@@ -36,18 +40,29 @@ class HierarchyNavigation extends Component{
     }
 
     private renderTree (data, target, options) {
+        target.selectAll('.show-more.hierarchy').remove();
+        target.selectAll('.show-more.instance').remove();
         var list = target.append('ul');
         Object.keys(data).forEach((el) => {
             var li = list.append('li').classed('tsi-leaf', data[el].isLeaf)
                 .classed('tsi-leafParent', data[el].isLeaf && options.withContextMenu);
 
             if(el === "Show More Hierarchies") {
-                li.append('a').classed('tsi-markedName hierarchy', true).attr('style', `margin-left: ${(data[el].level - 1) * 18}px`).html(el).on('click', data[el].onClick);
+                li.append('span').classed('tsi-markedName hierarchy show-more', true).attr('style', `padding-left: ${(data[el].level + 1) * 18}px`).html(el).on('click', data[el].onClick);
             } else if (el === "Show More Instances") {
-                li.append('a').classed('tsi-markedName instance', true).attr('style', `margin-left: ${(data[el].level - 1) * 18}px`).html(el).on('click', data[el].onClick);
+                li.append('span').classed('tsi-markedName instance show-more', true).attr('style', `padding-left: ${(data[el].level + 1) * 18}px`).html(el).on('click', data[el].onClick);
             } else {
-                li.append('span').classed('tsi-caret', true).attr('style', `left: ${(data[el].level - 1) * 18}px`);
-                li.append('span').classed('tsi-markedName', true).attr('style', `margin-left: ${(data[el].level - 1) * 18}px`).html(el).on('click', () => {data[el].isExpanded ? data[el].collapse() : data[el].expand()});
+                li.append('span').classed('tsi-caret', true).attr('style', `left: ${(data[el].level) * 18}px`);
+                li.append('span').classed('tsi-markedName', true).attr('style', `padding-left: ${(data[el].level + 1) * 18}px`).html(el).on('click', () => {
+                    if (data[el].onClick) {
+                        d3.event.stopPropagation();
+                        this.closeContextMenu();
+                        this.clickedInstance = data[el]; 
+                        data[el].onClick();
+                    } else {
+                        data[el].isExpanded ? data[el].collapse() : data[el].expand()
+                    }
+                });
             }
             data[el].node = li;
             data[el].isExpanded = false;
@@ -58,13 +73,11 @@ class HierarchyNavigation extends Component{
         let self = this;
         getToken().then(token => {
             self.server.getTimeseriesInstancesPathSearch(token, envFqdn, searchText, path, instancesPageSize, hierarchyiesPageSize, instancesContinuationToken, hierarchiesContinuationToken).then(r => {
-                let hierarchyData = {};
-                target.selectAll('a.hierarchy').remove();
-                target.selectAll('a.instance').remove(); 
+                let hierarchyData = {}; 
                 if (r.hierarchyNodes.hits.length > 0) {
                     r.hierarchyNodes.hits.forEach((h) => {
                         let hierarchy = new HierarchyNode(h.name, path);
-                        hierarchy.expand = () => {hierarchy.isExpanded = true; hierarchy.node.classed('tsi-expanded', true); self.pathSearch(getToken, envFqdn, searchText, hierarchy.path, hierarchy.node)};
+                        hierarchy.expand = () => {self.pathSearch(getToken, envFqdn, searchText, hierarchy.path, hierarchy.node); hierarchy.isExpanded = true; hierarchy.node.classed('tsi-expanded', true);};
                         hierarchy.collapse = () => {hierarchy.isExpanded = false; hierarchy.node.classed('tsi-expanded', false); hierarchy.node.selectAll('ul').remove();};
                         hierarchyData[(h.name === "" ? "(Empty)" : h.name) + " (" + h.cumulativeInstanceCount + ")"] = hierarchy;
                     });
@@ -85,9 +98,16 @@ class HierarchyNavigation extends Component{
                     hierarchyData[showMoreInstances.name] = showMoreInstances;
                 }
                 this.renderTree(hierarchyData, target, {...this.chartOptions, withContextMenu: true});
-            });
-        })
-    } 
+            })
+        });
+    }
+
+    private closeContextMenu() {
+        if(this.clickedInstance && this.clickedInstance.contextMenu) {
+            this.clickedInstance.contextMenu.remove();
+            d3.selectAll('li.selected').classed('selected', false);
+        }
+    }
 }
 
 function HierarchyNode (name, parentPath) {
@@ -104,7 +124,16 @@ function InstanceNode (tsId, name = null, typeId, contextMenuFunc, level) {
     this.timeSeriesId = tsId;
     this.name = name;
     this.typeId = typeId;
-    this.onClick = contextMenuFunc();
+    this.onClick = () => {
+        this.node.classed('selected', true);
+        let contextMenuActions = contextMenuFunc();
+        this.contextMenu = this.node.append('div').classed('tsi-hierarchyNavigationContextMenu', true);
+        var contextMenuList = this.contextMenu.append('ul');
+        contextMenuActions.forEach((a) => {
+            var option = Object.keys(a)[0];
+            contextMenuList.append('li').html(option).on('click', a[option]);
+        });
+    };
     this.isLeaf = true;
     this.level = level;
     this.node = null;
