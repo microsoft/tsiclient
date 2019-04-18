1beaf4f07a9816e297ed82c38d1e076c76fa9204
matdarMsft <matdar@microsoft.com>
2019-04-17 18:12:46 -0700

    colors for dotted lines (#87)
    * colors for dotted lines
    
    * new ellipsis icons and dotted line is proper color

--- src/UXClient/Components/LineChart/LineChart.ts
+++ src/UXClient/Components/LineChart/LineChart.ts
@@ -1140,6 +1140,7 @@ class LineChart extends ChartComponent {
                     .duration(durationFunction)
                     .ease(d3.easeExp)                                         
                     .attr("stroke-dasharray","5,5")      
+                    .attr("stroke", splitByColors[j])
                     .attrTween('d', function (d) {
                         var previous = d3.select(this).attr('d');
                         var current = aggLine(d);
