b4b1e298ba080ef94dba2bfe4eb72a71062b5c67
Nick Usoff <niusoff@microsoft.com>
2019-06-04 15:26:49 -0700

    fixed issue with availability chart axis getting overwritten

--- src/UXClient/Interfaces/TemporalXAxisComponent.ts
+++ src/UXClient/Interfaces/TemporalXAxisComponent.ts
@@ -12,6 +12,7 @@ class TemporalXAxisComponent extends ChartComponent {
     protected chartWidth;
     protected chartHeight;
     private smartTickFormat;
+    private xAxisEntered;
 
 	constructor(renderTarget: Element){
 		super(renderTarget);
@@ -27,7 +28,7 @@ class TemporalXAxisComponent extends ChartComponent {
         return xCopy;
     }
 
-    protected createXAxis (singleLineXAxisLabel) {
+    private createXAxis (singleLineXAxisLabel, snapFirst = false, snapLast = false) {
         let offsetX: any = this.createOffsetXAxis();
         let ticks = offsetX.ticks(this.getXTickNumber(singleLineXAxisLabel));
         ticks = ticks.map((d) => {
@@ -37,13 +38,20 @@ class TemporalXAxisComponent extends ChartComponent {
             ticks = this.x.domain();
         }
 
+        if (snapFirst) {
+            ticks[0] = this.x.domain()[0];
+        }
+        if (snapLast) {
+            ticks[ticks.length - 1] = this.x.domain()[1];
+        }
+
         this.smartTickFormat = this.createSmartTickFormat(ticks, offsetX);
         return d3.axisBottom(this.x)
             .tickValues(ticks)
             .tickFormat(Utils.timeFormat(this.labelFormatUsesSeconds(ticks), this.labelFormatUsesMillis(ticks), this.chartOptions.offset, this.chartOptions.is24HourTime, null, null));
     }
 
-    public getXTickNumber (singleLineXAxisLabel) {
+    private getXTickNumber (singleLineXAxisLabel) {
         return Math.max((singleLineXAxisLabel ? Math.floor(this.chartWidth / 300) :  Math.floor(this.chartWidth / 160)), 1);
     }
 
@@ -57,41 +65,51 @@ class TemporalXAxisComponent extends ChartComponent {
         return !this.chartOptions.minutesForTimeLabels && tickSpanSubSecond;
     }
 
-    protected drawXAxis (yOffset) {
-        var xAxisEntered = this.xAxis.enter()
-            .append("g")
-            .attr("class", "xAxis")
-            .merge(this.xAxis)
-            .attr("transform", "translate(0," + yOffset + ")")
-            .call(this.createXAxis(this.chartOptions.singleLineXAxisLabel));
+    public updateXAxis (forceFirst = false, forceLast = false) {
+        this.xAxisEntered.call(this.createXAxis(this.chartOptions.singleLineXAxisLabel, forceFirst, forceLast));
+        this.updateAxisText(forceFirst, forceLast);
+    }
 
+    private updateAxisText (forceFirst = false, forceLast = false) {
         //update text by applying function
         if (this.chartOptions.xAxisTimeFormat) {
-            let indexOfLast = xAxisEntered.selectAll('.tick').size() - 1;
+            let indexOfLast = this.xAxisEntered.selectAll('.tick').size() - 1;
             let self = this;
-            xAxisEntered.selectAll('.tick').each(function (d, i) {
+            this.xAxisEntered.selectAll('.tick').each(function (d, i) {
                 d3.select(this).select('text').text((d) => {
                     let momentTimeFormatString: string = String(self.chartOptions.xAxisTimeFormat(d, i, i === 0, i === indexOfLast));
                     return Utils.timeFormat(self.labelFormatUsesSeconds(), self.labelFormatUsesMillis(), self.chartOptions.offset, self.chartOptions.is24HourTime, null, momentTimeFormatString)(d);
                 });
             });
         } else {
-            let indexOfLast = xAxisEntered.selectAll('.tick').size() - 1;
+            let indexOfLast = this.xAxisEntered.selectAll('.tick').size() - 1;
             let self = this;
-            xAxisEntered.selectAll('.tick').each(function (d, i) {
+            this.xAxisEntered.selectAll('.tick').each(function (d, i) {
                 d3.select(this).select('text').text((d) => {
                     let momentTimeFormatString: string = String(self.smartTickFormat(d, i, i === 0, i === indexOfLast));
+                    //harcode format of first and last to include hours/minutes if force first/last
+                    if ((i === 0 && forceFirst) || (i === indexOfLast && forceLast)) { 
+                        momentTimeFormatString = Utils.subDateTimeFormat(self.chartOptions.is24HourTime, false, false) + ' L'; 
+                    }
                     return Utils.timeFormat(self.labelFormatUsesSeconds(), self.labelFormatUsesMillis(), self.chartOptions.offset, self.chartOptions.is24HourTime, null, momentTimeFormatString)(d);
                 });
             });
         }
 
-
-
         if (!this.chartOptions.singleLineXAxisLabel)                                     
-            xAxisEntered.selectAll('text').call(Utils.splitTimeLabel);
+            this.xAxisEntered.selectAll('text').call(Utils.splitTimeLabel);
 
-        xAxisEntered.select(".domain").style("display", "none");
+        this.xAxisEntered.select(".domain").style("display", "none");
+    }
+
+    protected drawXAxis (yOffset, snapFirst = false, snapLast = false) {
+        this.xAxisEntered = this.xAxis.enter()
+            .append("g")
+            .attr("class", "xAxis")
+            .merge(this.xAxis)
+            .attr("transform", "translate(0," + yOffset + ")")
+            .call(this.createXAxis(this.chartOptions.singleLineXAxisLabel));
+        this.updateAxisText();
     }
 
     private isSameDate (d1, d2) {
