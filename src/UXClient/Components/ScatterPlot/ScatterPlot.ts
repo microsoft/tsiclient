c35f53daa320fc9c3b9b95498bc04e44fdb4c91c
Cory Crowley <t-cocr@microsoft.com>
2019-07-11 11:42:03 -0700

    Fixed single data point padding check

--- src/UXClient/Components/ScatterPlot/ScatterPlot.ts
+++ src/UXClient/Components/ScatterPlot/ScatterPlot.ts
@@ -261,8 +261,8 @@ class ScatterPlot extends ChartComponent {
         let yExtentRange = this.chartComponentData.extents[this.yMeasure][1] - this.chartComponentData.extents[this.yMeasure][0];
 
         // Pad extents
-        let xOffset = (20 / this.chartWidth) * (xExtentRange);
-        let yOffset = (20 / this.chartHeight) * (yExtentRange);
+        let xOffset = (20 / this.chartWidth) * (xExtentRange == 0 ? 1 : xExtentRange);
+        let yOffset = (20 / this.chartHeight) * (yExtentRange == 0 ? 1: yExtentRange);
 
         let rOffset = null;
 
