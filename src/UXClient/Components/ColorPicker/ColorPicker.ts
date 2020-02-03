e245491e5e34be7cefeebd45b46dc9979a7cf291
Irem Aydin <iremay@microsoft.com>
2020-02-03 12:08:57 -0800

    default color and selected color getter added

--- src/UXClient/Components/ColorPicker/ColorPicker.ts
+++ src/UXClient/Components/ColorPicker/ColorPicker.ts
@@ -6,6 +6,7 @@ import './ColorPicker.scss';
 class ColorPicker extends Component{
     private colorPickerOptions = new ColorPickerOptions();
     private colorPickerElem;
+    private selectedColor;
 
     constructor(renderTarget: Element){
         super(renderTarget);
@@ -14,20 +15,22 @@ class ColorPicker extends Component{
     public render (colorPickerOptions: any) {
         this.colorPickerOptions.setOptions(colorPickerOptions);
         let colors = Utils.generateColors(this.colorPickerOptions.numberOfColors);
+        this.selectedColor = this.colorPickerOptions.defaultColor ? this.colorPickerOptions.defaultColor : colors[0];
 
         this.colorPickerElem = d3.select(this.renderTarget).classed("tsi-colorPicker", true);
+        this.colorPickerElem.text(''); 
         super.themify( this.colorPickerElem, this.colorPickerOptions.theme);
 
         // color selection button
         let colorPickerButton =  this.colorPickerElem.append('button').classed("tsi-colorPickerButton", true)
                     .on('click', this.showColorGrid);
-        colorPickerButton.append('div').classed("tsi-selectedColor", true).style("background-color", colors[0]);
-        colorPickerButton.append('span').classed("tsi-selectedColorValue", true).classed("hidden", this.colorPickerOptions.isColorValueHidden).text(colors[0]);
+        colorPickerButton.append('div').classed("tsi-selectedColor", true).style("background-color", this.selectedColor);
+        colorPickerButton.append('span').classed("tsi-selectedColorValue", true).classed("hidden", this.colorPickerOptions.isColorValueHidden).text(this.selectedColor);
 
         // color grid
         let colorGridElem =  this.colorPickerElem.append('div').classed("tsi-colorGrid", true);
         colors.forEach(c => {
-            let gridItem = colorGridElem.append('div').classed("tsi-colorItem", true)
+            let gridItem = colorGridElem.append('div').classed("tsi-colorItem", true).classed("selected", c === this.selectedColor)
                     .style("background-color", c)
                     .on('click', () => {this.colorPickerOptions.onColorClick(c); this.hideColorGrid(); this.setSelectedColor(c, gridItem)});
         });
@@ -39,6 +42,10 @@ class ColorPicker extends Component{
                 this.hideColorGrid();
             }
         });
+    }
+
+    public getSelectedColorValue = () => {
+        return this.selectedColor;
     }
 
     private showColorGrid = () => {
@@ -54,17 +61,20 @@ class ColorPicker extends Component{
         this.colorPickerElem.select(".tsi-selectedColorValue").text(cStr);
         this.colorPickerElem.select(".tsi-colorItem.selected").classed("selected", false);
         gridItem.classed("selected", true);
+        this.selectedColor = cStr;
     }
 }
 
 class ColorPickerOptions {
     public numberOfColors = 10;
+    public defaultColor = "#008272";
     public theme = Theme.light;
     public isColorValueHidden = false;
     public onColorClick = colorHex => {};
 
     public setOptions = (options) => {
         this.numberOfColors = options.hasOwnProperty('numberOfColors') ? options.numberOfColors : this.numberOfColors;
+        this.defaultColor = options.hasOwnProperty('defaultColor') ? options.defaultColor : this.defaultColor;
         this.theme = options.hasOwnProperty('theme') && options.theme in Theme ? options.theme : this.theme;
         this.isColorValueHidden = options.hasOwnProperty('isColorValueHidden') ? options.isColorValueHidden : this.isColorValueHidden;
         if (options.hasOwnProperty('onColorClick')) {
