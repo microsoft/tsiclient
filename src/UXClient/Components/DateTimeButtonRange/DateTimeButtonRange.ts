e3d46036c8d2a76371496efdf526f3f34c596310
Matt Darsney <matdar@microsoft.com>
2020-04-07 15:45:47 -0700

    two small a11y fixes (#217)
    * two small a11y fixes
    
    * small formatting fix in DTBR

--- src/UXClient/Components/DateTimeButtonRange/DateTimeButtonRange.ts
+++ src/UXClient/Components/DateTimeButtonRange/DateTimeButtonRange.ts
@@ -64,21 +64,29 @@ class DateTimeButtonRange extends DateTimeButton {
         });
 
         this.dateTimeButton.on("click", () => {
-            this.chartOptions.dTPIsModal = true;
-            this.dateTimePickerContainer.style("display", "block");
-            this.dateTimePicker.render(this.chartOptions, minMillis, maxMillis, this.fromMillis, this.toMillis, (fromMillis, toMillis, offset, isRelative, currentQuickTime) => {
-                this.chartOptions.offset = offset;
+            if(this.dateTimePickerContainer.style("display") !== "none"){
+                this.onClose();  // close if already open
+            }
+            else{
+                this.chartOptions.dTPIsModal = true;
+                this.dateTimePickerContainer.style("display", "block");
+                this.dateTimePicker.render(this.chartOptions, minMillis, maxMillis, this.fromMillis, this.toMillis, 
+                    (fromMillis, toMillis, offset, isRelative, currentQuickTime) => {
+                        this.chartOptions.offset = offset;
 
-                this.fromMillis = fromMillis;
-                this.toMillis = toMillis;
+                        this.fromMillis = fromMillis;
+                        this.toMillis = toMillis;
 
-                this.setButtonText(fromMillis, toMillis, isRelative, currentQuickTime);
-                this.onSet(fromMillis, toMillis, offset);
-                this.onClose();
-            }, () => {
-                this.onClose();
-                this.onCancel();
-            });
+                        this.setButtonText(fromMillis, toMillis, isRelative, currentQuickTime);
+                        this.onSet(fromMillis, toMillis, offset);
+                        this.onClose();
+                    }, 
+                    () => {
+                        this.onClose();
+                        this.onCancel();
+                    }
+                );
+            }
         });
     }
 }
