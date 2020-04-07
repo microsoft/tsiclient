435aead60bd10e6e3229316ce6c6f628f09f7444
darsney <matdar@microsoft.com>
2020-04-07 15:41:18 -0700

    two small a11y fixes

--- src/UXClient/Interfaces/DateTimeButton.ts
+++ src/UXClient/Interfaces/DateTimeButton.ts
@@ -38,7 +38,7 @@ class DateTimeButton extends ChartComponent {
         if (!this.dateTimePickerContainer) {
             this.dateTimePickerContainer = dateTimeContainer.append('div').classed('tsi-dateTimePickerContainer', true)
                                             .attr('role', 'dialog')
-                                            .attr('aria-label', this.getString('a time selection control dialog'));
+                                            .attr('aria-label', this.getString('a time selection control dialog.'));
             this.dateTimePickerContainer.style('display', 'none');
         }
         super.themify(d3.select(this.renderTarget), this.chartOptions.theme);        
