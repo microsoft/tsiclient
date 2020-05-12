68b1afad8013cdb785d405235da98f6639a4331e
niusoff <35545215+niusoff@users.noreply.github.com>
2020-05-11 17:31:53 -0700

    fix issue with null values brikcing the data table (#225)

--- src/UXClient/Utils.ts
+++ src/UXClient/Utils.ts
@@ -841,18 +841,20 @@ class Utils {
             let dataGroupKeyedObject = {};
             dataGroupKeyedObject[dGName] = convertedDataGroup;
             convertedData.push(dataGroupKeyedObject);
-            Object.keys(dataGroup).forEach((seriesName) => {
-                convertedDataGroup[seriesName] = {};
-                Object.keys(dataGroup[seriesName]).forEach((rawTS: string) => {
-                    let isoString: string;
-                    try {
-                        isoString = (new Date(rawTS)).toISOString();
-                        convertedDataGroup[seriesName][isoString] = dataGroup[seriesName][rawTS]; 
-                    } catch(RangeError) {
-                        console.log(`${rawTS} is not a valid ISO time`);
-                    }
+            if (dataGroup) {
+                Object.keys(dataGroup).forEach((seriesName) => {
+                    convertedDataGroup[seriesName] = {};
+                    Object.keys(dataGroup[seriesName]).forEach((rawTS: string) => {
+                        let isoString: string;
+                        try {
+                            isoString = (new Date(rawTS)).toISOString();
+                            convertedDataGroup[seriesName][isoString] = dataGroup[seriesName][rawTS];
+                        } catch(RangeError) {
+                            console.log(`${rawTS} is not a valid ISO time`);
+                        }
+                    });
                 });
-            });
+            }
         });
         return convertedData;
     }
