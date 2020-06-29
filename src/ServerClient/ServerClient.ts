ea6a77e554bb67bd23d44f7f0f32f9fa75fb6329
jl-g <37092114+jl-g@users.noreply.github.com>
2020-06-29 15:28:11 -0700

    Adding method to update a saved query (#243)
    * Adding method to update a saved query
    
    * Fixing request url

--- src/ServerClient/ServerClient.ts
+++ src/ServerClient/ServerClient.ts
@@ -249,6 +249,12 @@ class ServerClient {
         return this.createPromiseFromXhr(uri, "POST", payload, token, (responseText) => {return JSON.parse(responseText);});
     }
 
+    public updateSavedQuery(token: string, timeSeriesQuery: any, endpoint: string = 'https://api.timeseries.azure.com') {
+        let uri = `${endpoint}/artifacts/${timeSeriesQuery.id}${this.tsmTsqApiVersion}`;
+        let payload = JSON.stringify(timeSeriesQuery);
+        return this.createPromiseFromXhr(uri, "POST", payload, token, (responseText) => {return JSON.parse(responseText);});
+    }
+
     public getTimeseriesHierarchies(token: string, environmentFqdn: string) {
         let uri = 'https://' + environmentFqdn + '/timeseries/hierarchies/' + this.tsmTsqApiVersion;
         return this.createPromiseFromXhr(uri, "GET", {}, token, (responseText) => {return JSON.parse(responseText);});
