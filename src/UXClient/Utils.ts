017ad5ce0f55a141d3ec50924335fcbde3addd9e
Nick Usoff <niusoff@microsoft.com>
2020-07-14 21:04:20 -0700

    added handling for null values in standardizeTSStrings

--- src/UXClient/Utils.ts
+++ src/UXClient/Utils.ts
@@ -867,20 +867,18 @@ class Utils {
             let dataGroupKeyedObject = {};
             dataGroupKeyedObject[dGName] = convertedDataGroup;
             convertedData.push(dataGroupKeyedObject);
-            if (dataGroup) {
-                Object.keys(dataGroup).forEach((seriesName) => {
-                    convertedDataGroup[seriesName] = {};
-                    Object.keys(dataGroup[seriesName]).forEach((rawTS: string) => {
-                        let isoString: string;
-                        try {
-                            isoString = (new Date(rawTS)).toISOString();
-                            convertedDataGroup[seriesName][isoString] = dataGroup[seriesName][rawTS];
-                        } catch(RangeError) {
-                            console.log(`${rawTS} is not a valid ISO time`);
-                        }
-                    });
+            dataGroup && Object.keys(dataGroup).forEach((seriesName) => {
+                convertedDataGroup[seriesName] = {};
+                dataGroup[seriesName] && Object.keys(dataGroup[seriesName]).forEach((rawTS: string) => {
+                    let isoString: string;
+                    try {
+                        isoString = (new Date(rawTS)).toISOString();
+                        convertedDataGroup[seriesName][isoString] = dataGroup[seriesName][rawTS];
+                    } catch(RangeError) {
+                        console.log(`${rawTS} is not a valid ISO time`);
+                    }
                 });
-            }
+            });
         });
         return convertedData;
     }
