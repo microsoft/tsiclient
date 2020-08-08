16099acca4a60d0f419eefe9af3f210bef6d4a96
Cory Crowley <cocrowle@microsoft.com>
2020-08-07 17:30:23 -0700

    Fixed closed paths; checkout d3 curve types

--- src/UXClient/Components/ScatterPlot/ScatterPlot.ts
+++ src/UXClient/Components/ScatterPlot/ScatterPlot.ts
@@ -411,7 +411,7 @@ class ScatterPlot extends ChartVisualizationComponent {
         let line = d3.line()
             .x((d:any) => this.xScale(d.measures[this.xMeasure]))
             .y((d:any) => this.yScale(d.measures[this.yMeasure]))
-            .curve(d3.curveCatmullRomClosed) // apply smoothing to the line
+            .curve(d3.curveLinear) // apply smoothing to the line
 
         // Create lines
         for(let key in connectedSeries){
