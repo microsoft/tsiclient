3bfdee052d46ead9f42f8c2de6b492877b13d42c
Irem Gokce Aydin <45217314+iremgokce@users.noreply.github.com>
2020-10-23 15:37:52 -0700

    Accessibility bug fixes (#281)
    * accessibility bug fixes for hierarchy component aria roles and color picker unique ids
    
    * CR fixes

--- src/UXClient/Components/ColorPicker/ColorPicker.ts
+++ src/UXClient/Components/ColorPicker/ColorPicker.ts
@@ -4,16 +4,28 @@ import Utils from '../../Utils';
 import './ColorPicker.scss';
 import { KeyCodes } from '../../Constants/Enums';
 
-class ColorPicker extends Component{
-    private colorPickerElem;
-    private selectedColor;
-    private isColorGridVisible;
+interface ColorPickerOptions {
+    theme?: string,
+    numberOfColors?: number,
+    colors?: Array<string>,
+    defaultColor?: string,
+    isColorValueHidden?: boolean,
+    onSelect?: (colorHex: string) => void,
+    onClick?: (event: any) => void
+};
 
-    constructor(renderTarget: Element){
+class ColorPicker extends Component{
+    private colorPickerElem: any;
+    private selectedColor: string | null;
+    private isColorGridVisible: boolean;
+    private componentId: string;
+
+    constructor(renderTarget: Element, componentId: string = Utils.guid()){
         super(renderTarget);
+        this.componentId = componentId;
     }
 
-    public render (options: any = {}) {
+    public render (options: ColorPickerOptions = {}) {
         this.chartOptions.setOptions(options);
         this.selectedColor = this.chartOptions.defaultColor;
         if (this.chartOptions.colors.indexOf(this.selectedColor) === -1) {
@@ -26,17 +38,17 @@ class ColorPicker extends Component{
 
         // color selection button
         let colorPickerButton = this.colorPickerElem.append('button').classed("tsi-colorPickerButton", true)
-                    .attr("title", this.getString('Select color'))
-                    .attr("aria-label", this.getString('Select color'))
-                    .attr("aria-describedby", "tsi-selectedColorValue")
-                    .attr("aria-controls", "tsi-colorGrid")
-                    .on('click', () => {
-                        if (this.isColorGridVisible) {
-                            this.hideColorGrid(true);
-                        } else {
-                            this.chartOptions.onClick(d3.event); this.showColorGrid();
-                        }
-                    });
+            .attr("title", this.getString('Select color'))
+            .attr("aria-label", this.getString('Select color'))
+            .attr("aria-describedby", `tsi-selectedColorValue_${this.componentId}`)
+            .attr("aria-controls", `tsi-colorGrid_${this.componentId}`)
+            .on('click', () => {
+                if (this.isColorGridVisible) {
+                    this.hideColorGrid(true);
+                } else {
+                    this.chartOptions.onClick(d3.event); this.showColorGrid();
+                }
+            });
         if (this.selectedColor) {
             colorPickerButton.append('div').classed("tsi-selectedColor", true).style("background-color", this.selectedColor);
         } else {
@@ -44,43 +56,43 @@ class ColorPicker extends Component{
         }
 
         colorPickerButton.append('span').classed("tsi-selectedColorValue", true).classed("hidden", this.chartOptions.isColorValueHidden)
-                            .attr("id", "tsi-selectedColorValue")
-                            .text(this.selectedColor ? this.selectedColor : this.getString('No color'));
+            .attr("id", `tsi-selectedColorValue_${this.componentId}`)
+            .text(this.selectedColor ? this.selectedColor : this.getString('No color'));
 
         // color grid
-        let colorGridElem =  this.colorPickerElem.append('div').classed("tsi-colorGrid", true).attr("id", "tsi-colorGrid").attr("role", "grid");
+        let colorGridElem =  this.colorPickerElem.append('div').classed("tsi-colorGrid", true).attr("id", `tsi-colorGrid_${this.componentId}`).attr("role", "grid");
         let colorGridRowElem = colorGridElem.append('div').classed("tsi-colorGridRow", true).attr("role", "row");
         this.chartOptions.colors.forEach((c, idx) => {
             let gridItem = colorGridRowElem.append('div').classed("tsi-colorItem", true).classed("tsi-selected", c === this.selectedColor)
-                    .attr("tabindex", 0)
-                    .attr("role", "gridcell")
-                    .attr("aria-label", c)
-                    .attr("aria-selected", c === this.selectedColor)
-                    .style("background-color", c)
-                    .on('click', () => {
+                .attr("tabindex", 0)
+                .attr("role", "gridcell")
+                .attr("aria-label", c)
+                .attr("aria-selected", c === this.selectedColor)
+                .style("background-color", c)
+                .on('click', () => {
+                    d3.event.preventDefault();
+                    d3.event.stopPropagation();
+                    this.chartOptions.onSelect(c); this.hideColorGrid(true); this.setSelectedColor(c, gridItem);
+                })
+                .on('keydown', () => {
+                    if (d3.event.keyCode === KeyCodes.Tab && !d3.event.shiftKey && idx === this.chartOptions.colors.length - 1) { // tab
+                        d3.event.preventDefault();
+                        this.colorPickerElem.selectAll(".tsi-colorItem").nodes()[0].focus();
+                    } else if (d3.event.keyCode === KeyCodes.Enter) {
                         d3.event.preventDefault();
                         d3.event.stopPropagation();
-                        this.chartOptions.onSelect(c); this.hideColorGrid(true); this.setSelectedColor(c, gridItem);
-                    })
-                    .on('keydown', () => {
-                        if (d3.event.keyCode === KeyCodes.Tab && !d3.event.shiftKey && idx === this.chartOptions.colors.length - 1) { // tab
-                            d3.event.preventDefault();
-                            this.colorPickerElem.selectAll(".tsi-colorItem").nodes()[0].focus();
-                        } else if (d3.event.keyCode === KeyCodes.Enter) {
-                            d3.event.preventDefault();
-                            d3.event.stopPropagation();
-                            this.chartOptions.onSelect(c); 
-                            this.hideColorGrid(true); 
-                            this.setSelectedColor(c, gridItem);
-                        } else if (d3.event.keyCode === KeyCodes.Esc) {
-                            d3.event.preventDefault();
-                            d3.event.stopPropagation();
-                            this.hideColorGrid(true);
-                        }
-                    });
+                        this.chartOptions.onSelect(c); 
+                        this.hideColorGrid(true); 
+                        this.setSelectedColor(c, gridItem);
+                    } else if (d3.event.keyCode === KeyCodes.Esc) {
+                        d3.event.preventDefault();
+                        d3.event.stopPropagation();
+                        this.hideColorGrid(true);
+                    }
+                });
         });
 
-        d3.select("html").on("click." + Utils.guid(), () => {
+        d3.select("html").on("click." + this.componentId, () => {
             if (this.colorPickerElem.select(".tsi-colorPickerButton").filter(Utils.equalToEventTarget).empty() && 
                 this.colorPickerElem.select(".tsi-colorPickerButton").selectAll("*").filter(Utils.equalToEventTarget).empty() &&
                 this.colorPickerElem.selectAll(".tsi-colorGrid").filter(Utils.equalToEventTarget).empty()) {
