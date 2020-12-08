9a39b41b77a05ad5a6c0bb4848d79dc55ec62eaa
Nick Usoff <niusoff@microsoft.com>
2020-12-07 16:40:48 -0800

    adding check for empty filteredValues in getValuesAtTime, prevents sisue with call to interpolateValue when no values present in the marker

--- src/UXClient/Components/Marker/Marker.ts
+++ src/UXClient/Components/Marker/Marker.ts
@@ -393,6 +393,9 @@ class Marker extends Component {
                 let filteredValues = values.filter((v) => {
                     return (v.aggregateKey === aggKey && v.splitBy === splitBy && this.getValueOfVisible(v) !== null);
                 });
+                if (filteredValues.length === 0) {
+                    return;
+                }
                 if (filteredValues.length === 1 && (this.getValueOfVisible(filteredValues[0]) !== null)) {
                     valueArray.push(filteredValues[0]);
                 } else {
