74487dd0c747cda9a15b639bdb3ba01d934a7419
Irem Gokce Aydin <45217314+iremgokce@users.noreply.github.com>
2021-01-25 13:54:08 -0800

    hotfix for when there are multiple property values for the same timestamp, pick and preserve the non-null one (#309)

--- src/UXClient/UXClient.ts
+++ src/UXClient/UXClient.ts
@@ -183,7 +183,9 @@ class UXClient {
                 transformedAggregate[''] = {};
             else{
                 tsqr.timestamps.forEach((ts, j) => {
-                    aggregatesObject[ts] = tsqr.properties.reduce((p,c) => {p[c.name] = c['values'][j]; return p;}, {});
+                    aggregatesObject[ts] = tsqr.properties.reduce((p,c) => { // there can be multiple values for the same timestamp for a property, here we keep the latest non-null value if exist
+                        p[c.name] = aggregatesObject[ts] && aggregatesObject[ts][c.name] !== null ? aggregatesObject[ts][c.name] : c['values'][j]; return p;
+                    }, {});
                 }); 
             }
             result.push(transformedAggregate);
